f20377ac3d8b6bd8329c68732c23bce5b2a88b9e
ythlu <a-tehung@microsoft.com>
2018-05-27 09:08:06 +0800

    update to flow

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//建立名為photos資料庫的關聯（參考）
+var bwphotosRef = db.collection("bwphotos");
+//使用文件參考取得資料
+bwphotosRef.get().then(function (snapshot) {
+    //產生一個div 將內容替換成取得的圖檔資訊
+    //取得圖檔資訊 建立圖檔物件
+    snapshot.forEach(function (doc) {
+        var bwphoto = doc.data();
+        //建立顯示用的模板
+        var html =
+            `<div class="col" id="${doc.id}">
+<img src="${bwphoto.downloadURL}">
+</div>`;
+        //建立bwphoto元素
+        var $bwphoto = $(`<div class="col"></dvi>`);
+        $bwphoto.html(html);
+        //把div追加到bwphotos中
+        $("#bwphotos").append($bwphoto);
+    });
+});
